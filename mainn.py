6434f416e374f39708654cb9d415e023352a194b
peisje <darjakovalenko5@gmail.com>
2025-02-13 15:22:35 +0200

    Add files via upload

---
+++ mainn.py
@@ -0,0 +1,13 @@
+import esimene as esimeneFail
+import teinee as teineFail
+import kolmas as kolmasFail
+
+userInput = input("Milline ülesanne sa tahad ülevaadata")
+if userInput == "1":
+    esimeneFail.myFunc()
+elif userInput == "2":
+    teineFail.myPykkar()
+else:
+    pass
+    
+        
