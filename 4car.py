23e598d3b3439260ef80b9600a9492a6c64c683d
peisje <darjakovalenko5@gmail.com>
2025-03-19 14:43:36 +0200

    Add files via upload

---
+++ 4car.py
@@ -0,0 +1,31 @@
+class CarPark:
+    def __init__(self, name)
+        self.name = name
+    
+    
+class Car:
+    def __init__(self, brand,model,year)
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.is_broken = False
+    
+    def breakDown(self):
+        self.is_broken = True 
+    
+     car1 = Car("toyota", "1", "2020")
+     car2 = Car("volvo", "2", "2023")
+     car3 = Car("bentley", "3", "2024")
+    
+class ServiceCenter:
+    def __init__(self, name)
+        self.name = name
+        
+    def carRepair(self, car):
+    
+    
+class Owner:
+    def __init__(name)
+        self.name = name
+
+        
