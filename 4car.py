236bfdd248f77555e05b3f04704b4d4dc198d485
peisje <darjakovalenko5@gmail.com>
2025-03-19 14:54:46 +0200

    Update 4car.py

--- 4car.py
+++ 4car.py
@@ -1,31 +1,31 @@
-class CarPark:
-    def __init__(self, name)
-        self.name = name
-    
-    
 class Car:
-    def __init__(self, brand,model,year)
+    def __init__(self, brand, model, year):
         self.brand = brand
         self.model = model
         self.year = year
         self.is_broken = False
     
+class CarPark:
+    def __init__(self, name):
+        self.name = name
+        
     def breakDown(self):
         self.is_broken = True 
     
-     car1 = Car("toyota", "1", "2020")
-     car2 = Car("volvo", "2", "2023")
-     car3 = Car("bentley", "3", "2024")
-    
 class ServiceCenter:
-    def __init__(self, name)
+    def __init__(self, name):
         self.name = name
         
     def carRepair(self, car):
-    
+        car.is_broken = False
     
 class Owner:
-    def __init__(name)
+    def __init__(self, name):  
         self.name = name
 
+bmwCenter = ServiceCenter("BMW center")
+car1 = Car("BMW", "E34", 1990)
+car1.is_broken = True
         
+bmwCenter.carRepair(car1)
+print(car1.is_broken)
