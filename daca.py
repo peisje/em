17a6dc59a432c4c0aa24ceb4acffa96fe3c303b2
peisje <darjakovalenko5@gmail.com>
2025-04-16 10:06:20 +0300

    Update daca.py

--- daca.py
+++ daca.py
@@ -1,28 +1,60 @@
-Klass 1: Platvorm
-class Pl
-    def __init__(self):
-        self.ressursid = 100
+class Platform:
+    def __init__(self, food=100):
+        self.food = food  # Уровень начинается с 100 еды
 
-# Klass 2: Tase
-class Tase:
-    def __init__(self):
-        self.ressursid = 0
+    def add_food(self, amount):
+        self.food += amount  # Добавляем еду
 
-# Klass 3: Inimene
-class Inimene:
-    def __init__(self):
-        self.ressursid = 0
+    def remove_food(self, amount):
+        if self.food >= amount:
+            self.food -= amount  # Убираем еду
+        else:
+            print("Недостаточно еды!")
+
+    def get_info(self):
+        return f"Еда на уровне: {self.food}"
 
 
-# Loome objektid
-platvorm = Platvorm()
-tase = Tase()
-inimene = Inimene()
+class Level:
+    def __init__(self, platform):
+        self.platform = platform  # Привязываем платформу с едой к уровню
 
-# Platvorm annab ressursse tasemele
-print("Platvormil on", platvorm.ressursid, "ressurssi.")
-valik1 = input("Kas anda pool tasemele? (jah/ei): ")
+    def make_decision(self):
+        action = input("Что сделать? (оставить/передать/поделиться): ").strip().lower()
 
-if valik1 == "jah":
-    antud = platvorm.ressursid // 2
-    
+        if action == "оставить":
+            print("Еду оставили себе.")
+        elif action == "передать":
+            self.give_food_to_next_level()
+        elif action == "поделиться":
+            self.share_food()
+        else:
+            print("Некорректное действие!")
+
+    def give_food_to_next_level(self):
+        loss = self.platform.food * 0.2  # Потеря 20% при передаче
+        transferred_food = self.platform.food - loss
+        print(f"Передали еду на следующий уровень. Потеряно {loss} еды.")
+        self.platform.remove_food(self.platform.food)
+
+    def share_food(self):
+        shared_food = self.platform.food / 2
+        self.platform.remove_food(shared_food)
+        print(f"Поделились едой. Осталось {self.platform.food} еды.")
+
+
+# Создаём платформу с едой
+lvl1 = Platform(100)
+lvl2 = Platform(80)
+
+# Создаём уровни
+level1 = Level(lvl1)
+level2 = Level(lvl2)
+
+# Пример принятия решения на первом уровне
+level1.make_decision()
+print(lvl1.get_info())
+
+# Пример принятия решения на втором уровне
+level2.make_decision()
+print(lvl2.get_info())
