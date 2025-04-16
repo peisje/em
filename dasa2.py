966d514b34877d023e3fdc706a67c4db02087c5d
peisje <darjakovalenko5@gmail.com>
2025-04-16 09:39:24 +0300

    Create dasa2.py

---
+++ dasa2.py
@@ -0,0 +1,30 @@
+class Platform:
+    def __init__ (self):
+        self.food = 100
+        
+        
+    def addfood(self):
+        self.
+        
+    def removefood():
+        pass
+    
+    def getinfo():
+        pass
+    
+lvl1 = Platform(1)
+lvl1.addfood()
+
+class Level:
+    def __init__ (self, person):
+        self.person = person
+ 
+   def giveanotherlvl():
+         pass
+     
+ class People
+     def givefood():
+         pass
+     
+     def stayfood():
+         pass
