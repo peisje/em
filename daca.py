116e7b7edbe287a258e55eb4fd96e1868bd4c348
peisje <darjakovalenko5@gmail.com>
2025-04-16 09:50:51 +0300

    Update daca.py

--- daca.py
+++ daca.py
@@ -1,5 +1,5 @@
 Klass 1: Platvorm
-class Platvorm:
+class Pl
     def __init__(self):
         self.ressursid = 100
 
@@ -25,30 +25,4 @@ valik1 = input("Kas anda pool tasemele? (jah/ei): ")
 
 if valik1 == "jah":
     antud = platvorm.ressursid // 2
-    kaod = int(antud * 0.1)
-    tegelik = antud - kaod
-    platvorm.ressursid -= antud
-    tase.ressursid += tegelik
-    print("Platvorm andis tasemele", tegelik, "ressurssi.")
-else:
-    print("Platvorm ei andnud midagi.")
-
-# Tase annab ressursse inimesele
-print("\nTasemel on", tase.ressursid, "ressurssi.")
-valik2 = input("Kas anda pool inimesele? (jah/ei): ")
-
-if valik2 == "jah":
-    antud = tase.ressursid // 2
-    kaod = int(antud * 0.1)
-    tegelik = antud - kaod
-    tase.ressursid -= antud
-    inimene.ressursid += tegelik
-    print("Tase andis inimesele", tegelik, "ressurssi.")
-else:
-    print("Tase ei andnud midagi.")
-
-# Tulemused
-print("\n--- Lõpptulemused ---")
-print("Platvormil:", platvorm.ressursid)
-print("Tasemel:", tase.ressursid)
-print("Inimesel:", inimene.ressursid)
+    
