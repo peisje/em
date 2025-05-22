be954932e62978c13df28cd96b27fc2681e009ef
peisje <darjakovalenko5@gmail.com>
2025-05-22 11:44:04 +0300

    Add files via upload

---
+++ rekursioon.py
@@ -0,0 +1,74 @@
+# stack = []
+# 
+# for i in range(1,11):
+#     stack.append(f"{i} element")
+#     print(f"{i} element apended")
+#     for i in stack:
+#         print(i, end=" ")
+        
+        
+# for i in range(0,9):
+#     print(i, "hello world")
+ 
+#  Naide
+# def helloPrinter(n):
+#     if n <= 10:
+#         n+=1
+#         print("hello world")
+#         helloPrinter(n)
+#     else:
+#         return False
+#     
+# helloPrinter(0)
+
+# ul1
+# koradada string n korda
+
+# def helloPrinter(userInt, n):
+#     if n < userInt:
+#         n +=1
+#         helloPrinter(userInt, n)
+#     elif n==userInt:
+#         print("hello"*n)
+#         
+# helloPrinter(int(input("sisesta arv: ")),0)
+
+# ul2
+# kustutada  kõik ! meie lauses
+# def delete(words, n):
+#      if n>= len(words):
+#          return ""
+#      if words [n] == "!":
+#          return delete(words, n+1)
+#      else:
+#          return words[n]+ delete(words, n+1)
+#          n += 1
+# result = delete(input("sisesta tekst: "), 0)
+# print(result)
+
+# ul3
+# def find_upper(usinp, idx, 0):
+#         if idx >= len(usinp):
+#             return -1
+#         if text[index].isupper():
+#             return idx
+#         return find_upper(text, idx= 0)
+    
+# ul4
+
+# def leter(word):
+#      if not word:
+#         return
+#         print(word[0])
+#         leter(word[1:])
+# 
+# 
+# userInput=("test test")
+# leter(userInput)
+
+# ul5
+def delnum(word):
+
+
+
+
